3ea62bab0d506c8ddb82b27d9c8299d1f9577fb4
Nikolay Aleshkovskiy <tehlic@gmail.com>
2024-02-15 11:48:10 +0700

    fix: Remove wrongly added utils

--- example/packages/meteor-pagination/package-utils/index.js
+++
@@ -1,5 +0,0 @@
-import { PackageLogger } from './utils/PackageLogger';
-import { checkUnsupportedParams } from './utils/checkUnsupportedParams';
-import { isObject } from './utils/isObject';
-
-export { PackageLogger, checkUnsupportedParams, isObject };
